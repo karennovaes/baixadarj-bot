6e2b2cd5bfbb90a1e3621f4fe67a9eac01202f2f
Karen Novaes <karen-nd@outlook.com>
2023-08-01 02:02:06 -0300

    Retirado comandos duplicados do Index.js

--- index.js
+++ index.js
@@ -66,62 +66,6 @@ client.on(Events.InteractionCreate, async interaction =>{
 })
 
 
-const mysql = require('mysql')
-const canal_logs_WL = '1135766204859023412' // Canal de logs, coloque o ID do seu.
-
-client.on('interactionCreate', interaction => {
-   
-
-})
 
 
-client.on('modalSubmit', async (modal) => {
 
-    modal.member.roles.add("998968121731854468")
-
-    if (modal.customId === 'modal-wl') {
-
-        const id = modal.getTextInputValue('textinput_wl-id');
-        const idade = modal.getTextInputValue('textinput_wl-idade');
-        const nome = modal.getTextInputValue('textinput_wl-nome');
-
-        modal.member.setNickname(`${nome} | ${id}`)
-
-        const embed_log = new Discord.MessageEmbed()
-            .setColor('RED')
-            .setTitle(`Membro aprovado`)
-            .addField('ID', id)
-            .addField('Idade', idade)
-            .addField('Nome', nome)
-
-        // Parte do MYSQL, nao altere.
-        let connection = mysql.createPool({
-            host: '127.0.0.1', // IP da database. Se estiver em localhost deixe 127.0.0.1.
-            user: 'root', // Usuario da database, padrao root.
-            password: '', // Senha da database, padrao sem senha.
-            database: 'vrpex' // Nome da database.
-        });
-
-        let sql = `UPDATE vrp_users SET whitelisted = '1' WHERE id = '${id}'` // Dando set no banco de dados.
-        connection.query(sql, function (err, result) { // Query da conexao.
-            if (err) throw err; // Se houver algum erro, exibir no console.
-            // [LOG] Enviando a mensagem no canal definido.
-            const channel = client.channels.cache.get(canal_logs_WL)
-            channel.send({ embeds : [embed_log] })
-
-            // Enviando mensagem na DM do membro.
-            const user = modal.user;
-
-            const embed_dm = new Discord.MessageEmbed()
-            .setColor('GREEN')
-            .setAuthor(modal.guild.name)
-            .setDescription(`Parabéns ${modal.user}! Você \`passou\` no formulário e agora já pode se conectar ao servidor normalmente!`)
-            .setFooter(`connect ...... (IP da cidade para que o membro possa se conectar.)`)
-
-            user.send({ embeds: [embed_dm ]})
-
-            modal.reply({ content: `⚙ Vou te avisar no privado quanto ao resultado!`, ephemeral: true });
-        })
-
-    }
-})
